a6ba5cfaa85bc06e09c0214c8601bc8caa847a91
DESertyOne <105332132+DESertyOne@users.noreply.github.com>
2022-08-17 22:15:08 +0500

    Add work5

---
+++ PracticalWork5/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Tracing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticalWork5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+        public void WordDivision()
+        {
+            string word = Console.ReadLine();
+            string[] words = word.Split(' ');
+        }
+        public void WordOutput()
+        {
+            foreach (var less in WordDivision())
+                Console.WriteLine($"{less}");
+        }
+    }
+}
