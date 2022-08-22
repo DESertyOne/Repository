57c60b5d3f8a6bf3acf92aecd592fb29ea30552c
DESertyOne <105332132+DESertyOne@users.noreply.github.com>
2022-08-22 09:46:27 +0500

    Edit work 5.2

--- PracticalWork5.2/Program.cs
+++ PracticalWork5.2/Program.cs
@@ -1,15 +1,34 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+ 
 namespace PracticalWork5._2
 {
     internal class Program
     {
-        static void Main(string[] args)
+        public static void WordDivision()
         {
+            string word = Console.ReadLine();
+            string [] words = word.Split(' ');
+
+        }
+        public static void ReversWords(string inputPharse)
+        {
+            Console.ReadLine();
+            inputPharse.Reverse();
+            foreach (var revers in inputPharse)
+            {
+                Console.WriteLine($"{revers}");
+            }
+        }
+        private static void Main()
+        {
+            ReversWords("");
+            {
+                WordDivision();
+            }
+            Console.ReadKey();
         }
     }
 }
